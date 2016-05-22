87e83f01b01bd8b927d4676a54b8c2a73220f339
Ilter Cengiz <iltercengiz@yahoo.com>
2016-05-22 20:08:51 +0300

    Initial Commit

---
+++ Glickr/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Glickr
+//
+//  Created by Ilter Cengiz on 22/05/16.
+//  Copyright © 2016 Ilter Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
