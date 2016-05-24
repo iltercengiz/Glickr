3ef9ff907dc1614cb4ebea00849d210ebf973a17
Ilter Cengiz <iltercengiz@yahoo.com>
2016-05-24 22:10:57 +0300

    Recents endpoint call implementation

--- Glickr/Networking/FlickrAPIClient.swift
+++ Glickr/Networking/FlickrAPIClient.swift
@@ -7,7 +7,32 @@
 //
 
 import Foundation
+import Alamofire
+import AlamofireObjectMapper
+import Timberjack
 
 class FlickrAPIClient {
+    
+    static private let manager: Manager = {
+        let configuration = Timberjack.defaultSessionConfiguration()
+        let manager = Manager(configuration: configuration)
+        return manager
+    }()
+    
+    static func recentPhotos(page: Int,
+                             handler: (photos: [Photo], page: Int, pages: Int, total: Int) -> Void,
+                             failure: (error: NSError?) -> Void = { _ in }) {
+        manager.request(PhotosRouter.Recents(page: page)).responseObject {
+            (response: Alamofire.Response<PagedPhotosResponse, NSError>) in
+            if let photos = response.result.value?.photos,
+               let page = response.result.value?.page,
+               let pages = response.result.value?.pages,
+               let total = response.result.value?.total {
+                handler(photos: photos, page: page, pages: pages, total: total)
+            } else {
+                failure(error: response.result.error)
+            }
+        }
+    }
     
 }
